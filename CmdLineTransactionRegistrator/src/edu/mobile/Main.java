21cc95b7d18b7441d35326d1fba65371150c9036
Jakub Adamczyk <222684@edu.p.lodz.pl>
2022-10-20 16:28:23 +0200

    Initial revision

--- CmdLineTransactionRegistrator/src/edu/mobile/Main.java
+++ CmdLineTransactionRegistrator/src/edu/mobile/Main.java
@@ -1,5 +1,8 @@
+package edu.mobile;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
     }
 }
