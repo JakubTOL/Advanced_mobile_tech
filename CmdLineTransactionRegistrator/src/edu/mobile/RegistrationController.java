915a5ad70a051ab328b0350e7a1d6accf40237aa
Jakub Adamczyk <222684@edu.p.lodz.pl>
2022-10-20 18:30:18 +0200

    Completed TODO
    Signed-off-by: Jakub Adamczyk <222684@edu.p.lodz.pl>

--- CmdLineTransactionRegistrator/src/edu/mobile/RegistrationController.java
+++ CmdLineTransactionRegistrator/src/edu/mobile/RegistrationController.java
@@ -4,41 +4,12 @@ public class RegistrationController
 {
     public static void main(String[] args)
     {
-        /*
-        //quick reminder form MS_Teams classes
-        //System.out.println("Hello world!");
-        int a = 10;
-        long b = 1_000_000;
-        float c = 1.1234f;
-        double d = 1.4;
-        boolean e = true, f = false;
-        char g = 'a';
-        String h = "my_text"; //string is an object and can't be modified
-        // h = "new_text" //will be another object, previous string 'h' won't be destroyed
-        Object i = new String("Some other new string"); //create new object in stack
-        String i_str = (String) i; //conversion from object to string
-
-        Object o = new String();
-
-        for (int k=0;k<2;k++)
-        {
-            System.out.println("Argument " + k + "=" + args[k]);
-        }
-
-         */
         Contractor seller = new Contractor("Microsoft","Software",1);
         System.out.println("Seller id is: " + seller.contractorId);
 
         Contractor buyer = new Contractor("Apple","Computers",0);
         System.out.println("Buyer id is: " + buyer.contractorId);
-    }
 
-    /*
-    //public method with no return
-    public static void helperRegistration()
-    {
-        //code should be putted here
     }
-    */
 }
 
--- CmdLineTransactionRegistrator/src/edu/mobile/Transaction.java
+++ CmdLineTransactionRegistrator/src/edu/mobile/Transaction.java
@@ -2,27 +2,49 @@ package edu.mobile;
 
 public class Transaction
 {
-    Contractor contractorA;
-    Contractor contractorB;
+    Contractor contractorA;// seller
+    Contractor contractorB;// buyer
     String status;// -1 - on creation, 0 - negotiation, 1 - in progress, 2 - completed
+    long order_quantity;
 
     //TODO: write constructor with 2 params (contractors)
-    public Transaction()
+    public Transaction(Contractor seller, Contractor buyer)
     {
-
+        this.contractorA = seller;
+        this.contractorB = buyer;
     }
     public void checkValidity()
     {
         //TODO: check if contractor "industry" is the same
         //TODO: check if provider has enough goods to sell
         //if ok: then set status to 0
+        if (contractorA.industry == contractorB.industry)
+        {
+            if (order_quantity < contractorA.inStoreAmount)
+            {
+                status = "Negotiation";
+                System.out.println("Current status is: " + status);
+            }
+            else
+            {
+                System.out.println("There is not enough goods to complete transaction.");
+            }
+        }
+        else
+        {
+            System.out.println("Contractors have different industries.");
+        }
     }
     public void initTransaction()
     {
         //set status to 1 and print to cmd
+        status = "In progress";
+        System.out.println(status);
     }
     public void completeTransaction()
     {
-        // set status to 0 and print to cmd
+        // set status to 2 and print to cmd
+        status = "Completed";
+        System.out.println(status);
     }
 }
