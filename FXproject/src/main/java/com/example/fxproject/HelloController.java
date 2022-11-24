b383bddd6cdedfe7a66b4802e4334f2677424104
Jakub Adamczyk <222684@edu.p.lodz.pl>
2022-11-24 16:31:15 +0100

    Just some fun with content

--- FXproject/src/main/java/com/example/fxproject/HelloController.java
+++ FXproject/src/main/java/com/example/fxproject/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Congrats, you have clicked the button :D !");
     }
 }
